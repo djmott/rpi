eb197bfa2ced1fba9c2e716b99d10adaf6c5408b
David Mott <davidm@ospreyvideo.com>
2017-04-19 07:08:11 -0500

    .

--- rpi.sh
+++ rpi.sh
@@ -14,13 +14,21 @@ main(){
 	export _B="$(tput setaf 4)"
 	export _Z="$(tput sgr0)"
   export _RPI_TARGET=${_RPI_TARGET:-armv8-rpi3-linux-gnueabihf}
-  export _RPI_TOOLCHAIN=${_RPI_TOOLCHAIN:-"$_RPI_DIR"/toolchain/build}
-  export _RPI_SYSROOT=${_RPI_SYSROOT:-"$_RPI_DIR"/toolchain/build/$_RPI_TARGET/sysroot}
+  export _RPI_TOOLCHAIN=${_RPI_TOOLCHAIN:-"$_RPI_DIR"/toolchain}
+  export _RPI_SYSROOT=${_RPI_SYSROOT:-"$_RPI_TOOLCHAIN"/$_RPI_TARGET/sysroot}
   export _RPI_ROOTFS=${_RPI_ROOTFS:-"$_RPI_DIR"/rootfs}
   export _RPI_TMP=${_RPI_TMP:-"$_RPI_DIR"/.tmp}
   export _RPI_DOWNLOADS=${_RPI_DOWNLOADS:-"$_RPI_DIR"/.download}
+  export PATH="$_RPI_TOOLCHAIN"/bin:$PATH
+  mkdir -p "$_RPI_ROOTFS"/{dev,proc,run,sys,usr/bin} "$_RPI_TMP" "$_RPI_DOWNLOADS"
+  set +e
+  for item in "$_RPI_TOOLCHAIN"/bin/$_RPI_TARGET*; do
+    local _target="$(basename $item)"
+    local _var=$(echo ${_target##*-} | tr [:lower:] [:upper:])
+    export "$_var"="$_target" 2>/dev/null
+  done
+  set -e
   trap _atexit INT TERM EXIT
-  mkdir -p $_RPI_ROOTFS $_RPI_TMP $_RPI_DOWNLOADS
   $*
   return 0
 }
@@ -28,21 +36,28 @@ main(){
 _atexit(){
   local _ret=$?
   if [ "0" != "$_ret" ]; then
-    echo -e "$_R  Exiting with code: $_ret $_Z"
-  else
-    printenv | sort | grep "^_RPI_" > "$_RPI_DIR"/.rpi.conf
+    echo -e "${_R}Exiting with code: $_ret $_Z"
+    exit $_ret
   fi
-  exit $_ret
+  set +e
+  printenv | sort | grep "^_RPI_" > "$_RPI_DIR"/.rpi.conf
+
+  exit 0
 }
+
 
 help(){
   echo -e "
 Usage: $0 $_G<command>$_Z
 
 Commands:
-  $_G help $_Z            - this information
-  $_G qemu $_Z            - enter emulated chroot
-  $_G shell $_Z           - enter dev shell
+  $_G help $_Z                - this information
+  $_G qemu $_Z                - enter emulated chroot
+  $_G shell $_Z               - enter dev shell
+  $_G bootstrap $_B<options>$_Z
+bootstrap options:
+    $_B toolchain $_Z         - build ct-ng toolchain
+    $_B rootfs $_Z            - core root file system
   "
 }
 
@@ -64,6 +79,18 @@ ${_Y}$(printenv | sort | grep "^_RPI_")${_Z}
   "
   
   bash -norc -noprofile
+}
+
+bootstrap(){
+  bootstrap-$*
+}
+
+bootstrap-toolchain(){
+  echo "bootstrap-toolchain"
+}
+
+bootstrap-rootfs(){
+  echo "bootstrap-rootfs"
 }
 
 main $*
